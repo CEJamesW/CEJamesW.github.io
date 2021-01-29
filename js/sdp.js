a246be9759a1e233cbcb568dc400b78b927980a6
ĺÔƅ¥¦ <cejames@163.com>
2021-01-29 15:27:24 +0800

    qaq

--- js/sdp.js
+++ js/sdp.js
@@ -1,49 +1,38 @@
-<!DOCTYPE html>
-<style>
-span {font:14px/20px 微软雅黑;}
-#counter {
- width:50px;height:20px;
- border:1px solid #CCC;
- background:#F9F9F9;
- font:14px/20px Consolas;
- text-align:center;
- margin:10px;
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = [37, 38, 39, 40];
+ 
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false; 
 }
-</style>
-<span>使用鼠标滚轮调整数值大小</span><br/>
-<div id="counter">0</div>
-<script>
-//判断浏览器
-var isIE=navigator.userAgent.match(/MSIE (\d)/i);
-isIE=isIE?isIE[1]:undefined;
-var isFF=/FireFox/i.test(navigator.userAgent);
-//获取元素
-var counter=document.getElementById("counter");
-//鼠标滚轮事件
-if(isIE<9) //传统浏览器使用MouseWheel事件
- counter.attachEvent("onmousewheel",function(){
-  //计算鼠标滚轮滚动的距离
-  //一格3行，每行40像素，所以除以120
-  var v=event.wheelDelta/120;
-  counter.innerHTML=counter.innerHTML*1+v;
-  //阻止浏览器默认方法
-  return false;
- });
-else if(!isFF) //除火狐外的现代浏览器也使用MouseWheel事件
- counter.addEventListener("mousewheel",function(e){
-  //计算鼠标滚轮滚动的距离
-  var v=e.wheelDelta/120;
-  counter.innerHTML=counter.innerHTML*1+v;
-  //阻止浏览器默认方法
-  e.preventDefault();
- },false);
-else //奇葩的火狐使用DOMMouseScroll事件
- counter.addEventListener("DOMMouseScroll",function(e){
-  //计算鼠标滚轮滚动的距离
-  //一格是3行，但是要注意，这里和像素不同的是它是负值
-  var v=-e.detail/3;
-  counter.innerHTML=counter.innerHTML*1+v;
-  //阻止浏览器默认方法
-  e.preventDefault();
- },false);
-</script>
+ 
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+ 
+function wheel(e) {
+  preventDefault(e);
+}
+ 
+function disable_scroll() {
+  if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
+  document.onkeydown = keydown;
+}
+ 
+function enable_scroll() {
+    if (window.removeEventListener) {
+        window.removeEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = document.onkeydown = null; 
+}
