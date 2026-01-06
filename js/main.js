b28d0a9901913e6b09cd0b938693dff8ae583cae
王斯民(Wang Simin) <cejames@163.com>
2026-01-06 22:33:11 +0800

    upload pages

--- js/main.js
+++ js/main.js
@@ -1,91 +1,277 @@
-window.onload = function(){
-	
-	var video = document.getElementById("video");
-	var lis = document.getElementsByTagName("li");
-	var vLen = lis.length; // 播放列表的长度
-	var url = [];
-	var ctrl = document.getElementById("playList-hidden");
-	var ctrl_show = document.getElementById('playList-show1');
-	var aside = document.getElementById("playList");
-	var curr = 1; // 当前播放的视频
-	
-	for(var i=0;i<lis.length;i++){
-		
-			url[i] = lis[i].getAttribute("value");
-			
-	}
-	
-	//绑定单击事件
-	for(var i=0;i<lis.length;i++){
-		
-			lis[i].onclick = function(){
-				for(var j=0;j<lis.length;j++){
-					if(lis[j] == this){
-						video.setAttribute("src",this.getAttribute("value"));
-						video.setAttribute('autoplay','autoplay');
-						this.innerHTML = 'palying '+this.innerHTML;
-						this.className = "select";
-						curr = j+1;
-					}else{
-						lis[j].innerHTML = lis[j].getAttribute("title");
-						lis[j].className = "";
-					}
-				}
-				
-			
-//			console.log(this.getAttribute("value"));  //调试代码
-		}
-			
-	}	
-	
-	//收起播放列表
-	ctrl.onclick = function(){
-		
-		aside.style.transition = "1s";
-		aside.style.transform = "translateX(-10vw)";
-		setTimeout(function(){
-			aside.style.display = "none";
-			ctrl_show.style.visibility= 'visible';
-		},"1000");
-	
-	}
-	
-	//展开播放列表
-	ctrl_show.onclick = function(){
-		aside.style.display = "block";
-		ctrl_show.style.visibility= 'hidden';
-		setTimeout(function(){
-			aside.style.transform = "translateX(0vw)";
-		},"0");
-	
-	}
+//弹窗样式
+iziToast.settings({
+    timeout: 10000,
+    progressBar: false,
+    close: false,
+    closeOnEscape: true,
+    position: 'topCenter',
+    transitionIn: 'bounceInDown',
+    transitionOut: 'flipOutX',
+    displayMode: 'replace',
+    layout: '1',
+    backgroundColor: '#00000040',
+    titleColor: '#efefef',
+    messageColor: '#efefef',
+    iconColor: '#efefef',
+});
 
-	video.setAttribute('src',url[0]);
-	lis[0].innerHTML = 'palying '+lis[0].innerHTML;
-	lis[0].className = "select";
-	
-	
-	
-	video.addEventListener('ended', play);
-	//play();
-	function play() {
-	   video.src = url[curr];
-	   video.load(); // 如果短的话，可以加载完成之后再播放，监听 canplaythrough 事件即可
-	   video.play();
-	   
-	   for(var j=0;j<lis.length;j++){
-			if(j == curr){
-				video.setAttribute("src",lis[j].getAttribute("value"));
-				video.setAttribute('autoplay','autoplay');
-				lis[j].innerHTML = 'palying '+lis[j].innerHTML;
-				lis[j].className = "select";
-			}else{
-				lis[j].innerHTML = lis[j].getAttribute("title");
-				lis[j].className = "";
-			}
-		}
-	   curr++;
-	   if (curr >= vLen) curr = 0; // 播放完了，重新播放
-	}
-	
+//加载完成后执行
+window.addEventListener('load', function () {
+
+    //载入动画
+    $('#loading-box').attr('class', 'loaded');
+    $('#bg').css("cssText", "transform: scale(1);filter: blur(0px);transition: ease 1.5s;");
+    $('.cover').css("cssText", "opacity: 1;transition: ease 1.5s;");
+    $('#section').css("cssText", "transform: scale(1) !important;opacity: 1 !important;filter: blur(0px) !important");
+
+    //用户欢迎
+    setTimeout(function () {
+        iziToast.show({
+            timeout: 2500,
+            title: hello,
+            message: '欢迎 ~'
+        });
+    }, 800);
+}, false)
+
+setTimeout(function () {
+    $('#loading-text').html("字体及文件加载可能需要一定时间")
+}, 3000);
+
+//延迟加载音乐播放器
+function downloadJSAtOnload() {
+    var element = document.createElement("script");
+    element.src = "./js/music.js";
+    document.body.appendChild(element);
+}
+if (window.addEventListener)
+    window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+    window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+
+//新春灯笼 （ 需要时取消注释 ）
+/*
+new_element=document.createElement("link");
+new_element.setAttribute("rel","stylesheet");
+new_element.setAttribute("type","text/css");
+new_element.setAttribute("href","./css/lantern.css");
+document.body.appendChild(new_element);
+
+new_element=document.createElement("script");
+new_element.setAttribute("type","text/javascript");
+new_element.setAttribute("src","./js/lantern.js");
+document.body.appendChild(new_element);
+*/
+
+//火狐浏览器独立样式
+if (isFirefox = navigator.userAgent.indexOf("Firefox") > 0) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.href = './css/firefox.css';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    head.appendChild(link);
+    window.addEventListener('load', function () {
+        iziToast.show({
+            timeout: 8000,
+            iconUrl: './img/icon/warn.png',
+            message: '您正在使用火狐浏览器，部分功能可能不支持'
+        });
+    }, false)
+}
+
+//获取一言
+fetch('https://v1.hitokoto.cn?max_length=24')
+    .then(response => response.json())
+    .then(data => {
+        $('#hitokoto_text').html(data.hitokoto)
+        $('#from_text').html(data.from)
+    })
+    .catch(console.error)
+
+//fetch('https://api.seniverse.com/v4?fields=air_daily&locations=39:118&key=SBxsr007qqxRIu3HG')
+//    .then(response => response.json())
+//    .then(data => {
+//        $('#wea_text').html(data.wea)
+//        $('#city_text').html(data.city)
+//        $('#tem_night').html(data.tem_night)
+//        $('#tem_day').html(data.tem_day)
+//        // $('#win_text').html(data.win)
+//        // $('#win_speed').html(data.win_speed)
+//    })
+//    .catch(console.error)
+
+//获取时间
+var t = null;
+t = setTimeout(time, 1000);
+
+function time() {
+    clearTimeout(t);
+    dt = new Date();
+    var y = dt.getYear() + 1900;
+    var mm = dt.getMonth() + 1;
+    var d = dt.getDate();
+    var weekday = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+    var day = dt.getDay();
+    var h = dt.getHours();
+    var m = dt.getMinutes();
+    var s = dt.getSeconds();
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+    //document.getElementById("time").innerHTML = y + "&nbsp;年&nbsp;" + mm + "&nbsp;月&nbsp;" + d + "&nbsp;日&nbsp;" + "<span class='weekday'>" + weekday[day] + "</span><br>" + "<span class='time-text'>" + h + ":" + m + ":" + s + "</span>";
+    $("#time").html(y + "&nbsp;年&nbsp;" + mm + "&nbsp;月&nbsp;" + d + "&nbsp;日&nbsp;" + "<span class='weekday'>" + weekday[day] + "</span><br>" + "<span class='time-text'>" + h + ":" + m + ":" + s + "</span>");
+    t = setTimeout(time, 1000);
+}
+
+//链接提示文字
+$("#social").mouseover(function () {
+    $("#social").css({
+        "background": "rgb(0 0 0 / 25%)",
+        'border-radius': '6px',
+        "backdrop-filter": "blur(5px)"
+    });
+    $("#link-text").css({
+        "display": "block",
+    });
+}).mouseout(function () {
+    $("#social").css({
+        "background": "none",
+        "border-radius": "6px",
+        "backdrop-filter": "none"
+    });
+    $("#link-text").css({
+        "display": "none"
+    });
+});
+
+$("#github").mouseover(function () {
+    $("#link-text").html("去 Github 看看");
+}).mouseout(function () {
+    $("#link-text").html("通过这里联系我");
+});
+$("#qq").mouseover(function () {
+    $("#link-text").html("有什么事吗");
+}).mouseout(function () {
+    $("#link-text").html("通过这里联系我");
+});
+$("#email").mouseover(function () {
+    $("#link-text").html("来封 Email");
+}).mouseout(function () {
+    $("#link-text").html("通过这里联系我");
+});
+$("#telegram").mouseover(function () {
+    $("#link-text").html("发封电报吧");
+}).mouseout(function () {
+    $("#link-text").html("通过这里联系我");
+});
+$("#twitter").mouseover(function () {
+    $("#link-text").html("都喜欢看什么");
+}).mouseout(function () {
+    $("#link-text").html("通过这里联系我");
+});
+
+//更多页面切换
+var shoemore = false;
+$('#switchmore').on('click', function () {
+    shoemore = !shoemore;
+    if (shoemore && $(document).width() >= 990) {
+        $('#container').attr('class', 'container mores');
+        $("#change").html("Oops&nbsp;!");
+        $("#change1").html("哎呀，这都被你发现了（ 再点击一次可关闭 ）");
+    } else {
+        $('#container').attr('class', 'container');
+        $("#change").html("Hello&nbsp;World&nbsp;!");
+        $("#change1").html("一个建立于 21 世纪的小站，存活于互联网的边缘");
+    }
+});
+
+//更多页面关闭按钮
+$('#close').on('click', function () {
+    $('#switchmore').click();
+});
+
+//移动端菜单栏切换
+var switchmenu = false;
+$('#switchmenu').on('click', function () {
+    switchmenu = !switchmenu;
+    if (switchmenu) {
+        $('#row').attr('class', 'row menus');
+        $("#menu").html("<i class='iconfont icon-times'></i>");
+    } else {
+        $('#row').attr('class', 'row');
+        $("#menu").html("<i class='iconfont icon-bars'>");
+    }
+});
+
+//更多弹窗页面
+$('#openmore').on('click', function () {
+    $('#box').css("display", "block");
+    $('#row').css("display", "none");
+    $('#more').css("cssText", "display:none !important");
+});
+$('#closemore').on('click', function () {
+    $('#box').css("display", "none");
+    $('#row').css("display", "flex");
+    $('#more').css("display", "flex");
+});
+
+//监听网页宽度
+window.addEventListener('load', function () {
+    window.addEventListener('resize', function () {
+        //关闭移动端样式
+        if (window.innerWidth >= 600) {
+            $('#row').attr('class', 'row');
+            $("#menu").html("<i class='iconfont icon-bars'>");
+            //移除移动端切换功能区
+            $('#rightone').attr('class', 'row rightone');
+        }
+
+        if (window.innerWidth <= 990) {
+            //移动端隐藏更多页面
+            $('#container').attr('class', 'container');
+            $("#change").html("Hello&nbsp;World&nbsp;!");
+            $("#change1").html("一个建立于 21 世纪的小站，存活于互联网的边缘");
+
+            //移动端隐藏弹窗页面
+            $('#box').css("display", "none");
+            $('#row').css("display", "flex");
+            $('#more').css("display", "flex");
+        }
+    })
+})
+
+//移动端切换功能区
+var changemore = false;
+$('#changemore').on('click', function () {
+    changemore = !changemore;
+    if (changemore) {
+        $('#rightone').attr('class', 'row menus mobile');
+    } else {
+        $('#rightone').attr('class', 'row menus');
+    }
+});
+
+//更多页面显示关闭按钮
+$("#more").hover(function () {
+    $('#close').css("display", "block");
+}, function () {
+    $('#close').css("display", "none");
+})
+
+//屏蔽右键
+document.oncontextmenu = function () {
+//    iziToast.show({
+//        timeout: 2000,
+//        iconUrl: './img/icon/warn.png',
+//        message: '为了浏览体验，本站禁用右键'
+//    });
+    return false;
 }
